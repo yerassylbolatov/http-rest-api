e216f3623e0ab5f153be6b7aa3adcf17e9951aec
yerassyl <y.bolatov1@gmail.com>
2022-08-13 22:24:27 +0600

    web-server

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,66 @@
+package apiserver
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/yerassylbolatov/http-rest-api/store"
+	"io"
+	"net/http"
+)
+
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+	store  *store.Store
+}
+
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+	s.configRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("Starting api server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
+	return nil
+}
+
+func (s *APIServer) configRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	return nil
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
+}
---
+++ store/store.go
@@ -0,0 +1,35 @@
+package store
+
+import "database/sql"
+import _ "github.com/lib/pq"
+
+type Store struct {
+	config *Config
+	db     *sql.DB
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		config: config,
+	}
+}
+
+func (s *Store) Open() error {
+	db, err := sql.Open("postgres", s.config.DatabaseUrl)
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	s.db = db
+
+	return nil
+}
+
+func (s *Store) Close() {
+	s.db.Close()
+}
